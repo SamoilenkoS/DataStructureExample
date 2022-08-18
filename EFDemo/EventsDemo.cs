b1e4da67df7308c7e0e686e3fc16f5cff3808eb0
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-08-18 21:58:45 +0300

    Added Event demo

---
+++ EFDemo/EventsDemo.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDemo
+{
+    public class EventsDemo
+    {
+        public class Printer
+        {
+            private Inc _blackInc;
+            public event Action<string> IncLeak;
+
+            public Printer()
+            {
+                Console.WriteLine("Initialize printer");
+                _blackInc = new Inc();
+            }
+
+            public void PrintDocument(Document document)
+            {
+                if(document.Length > _blackInc.CurrentAmount)
+                {
+                    IncLeak?.Invoke(nameof(_blackInc));
+                    return;
+                }
+
+                Console.WriteLine($"\tPrinting document\t\n" + document.Text + document.Title);
+                _blackInc.Use(document.Length);
+            }
+
+            public void Refill(string color)
+            {
+                Console.WriteLine($"Trying to refil {color} in printer");
+                switch (color)
+                {
+                    case "_blackInc":
+                        _blackInc.Refill();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public class Inc
+        {
+            private int _currentAmount;
+            public int CurrentAmount { get => _currentAmount; }
+
+            public Inc()
+            {
+                _currentAmount = 100;
+                Console.WriteLine($"Initialize inc. Current amount:{_currentAmount}");
+            }
+
+            public void Use(int amount)
+            {
+                _currentAmount -= amount;
+                Console.WriteLine($"{amount} inc used. Current amount:{_currentAmount}");
+            }
+
+            internal void Refill()
+            {
+                _currentAmount = 100;
+                Console.WriteLine("Inc refilled");
+            }
+        }
+
+        public class Document
+        {
+            public string Title { get; set; }
+            public string Text { get; set; }
+
+            public int Length => Title.Length + Text.Length;
+        }
+    }
+}
--- EFDemo/Program.cs
+++ EFDemo/Program.cs
@@ -6,6 +6,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using static EFDemo.EventsDemo;
 
 namespace EFDemo
 {
@@ -228,210 +229,24 @@ namespace EFDemo
 
             return result;
         }
-
+        static Printer printer = new Printer();
         static void Main()
         {
-            Car[] cars = new Car[3];
-            cars[0] = new Car();
-            cars[1] = new Ferrari();
-            cars[2] = new Gelly();
-
-            foreach (var car in cars)
-            {
-                Console.WriteLine($"{car} {car.GetMaxSpeed()}");
-            }
-
-            Console.WriteLine(cars[0].Sum("hello", 10, 20, 150, 40));
-
-            Console.WriteLine(cars[0].Sum("world!"));
-
-
             Random random = new Random();
-            string current = string.Empty;
-            //var response = RepeatActionUntilConditionIsFalse(
-            //    () => { current = GetData(); }, () => current == "OK");
 
-            ForChange forChange = new ForChange();
-            var fields = forChange.GetType().GetFields(
-                BindingFlags.NonPublic |
-               // BindingFlags.Static |
-                BindingFlags.Public |
-                BindingFlags.Instance |
-                BindingFlags.DeclaredOnly |
-                BindingFlags.GetProperty |
-                BindingFlags.SetProperty);
-            var properties = forChange.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public);
-            foreach(var field in fields)
+            printer.IncLeak += Printer_IncLeak;
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine(field);
+                var document = new Document();
+                document.Text = random.Next().ToString();
+                document.Title = random.Next().ToString();
+                printer.PrintDocument(document);
             }
+        }
 
-            var myFieldInfo = forChange.GetType().GetField("Test",
-            BindingFlags.NonPublic | BindingFlags.Instance);
-            //myFieldInfo.SetValue(forChange, 55);
-            forChange.Print();
-            // BindingFlags.InvokeMethod
-            // Call a static method.
-            Type t = typeof(TestClass);
-
-            Console.WriteLine();
-            Console.WriteLine("Invoking a static method.");
-            Console.WriteLine("-------------------------");
-            t.InvokeMember("SayHello", BindingFlags.InvokeMethod | BindingFlags.Public |
-                BindingFlags.Static, null, null, new object[] { });
-
-            // BindingFlags.InvokeMethod
-            // Call an instance method.
-            TestClass c = new TestClass();
-            Console.WriteLine();
-            Console.WriteLine("Invoking an instance method.");
-            Console.WriteLine("----------------------------");
-            c.GetType().InvokeMember("AddUp", BindingFlags.InvokeMethod, null, c, new object[] { });
-            c.GetType().InvokeMember("AddUp", BindingFlags.InvokeMethod, null, c, new object[] { });
-
-            // BindingFlags.InvokeMethod
-            // Call a method with parameters.
-            object[] args = new object[] { 100.09, 184.45 };
-            object result;
-            Console.WriteLine();
-            Console.WriteLine("Invoking a method with parameters.");
-            Console.WriteLine("---------------------------------");
-            result = t.InvokeMember("ComputeSum", BindingFlags.InvokeMethod, null, null, args);
-            Console.WriteLine("{0} + {1} = {2}", args[0], args[1], result);
-
-            // BindingFlags.GetField, SetField
-            Console.WriteLine();
-            Console.WriteLine("Invoking a field (getting and setting.)");
-            Console.WriteLine("--------------------------------------");
-            // Get a field value.
-            result = t.InvokeMember("Name", BindingFlags.GetField, null, c, new object[] { });
-            Console.WriteLine("Name == {0}", result);
-            // Set a field.
-            t.InvokeMember("Name", BindingFlags.SetField, null, c, new object[] { "NewName" });
-            result = t.InvokeMember("Name", BindingFlags.GetField, null, c, new object[] { });
-            Console.WriteLine("Name == {0}", result);
-
-            Console.WriteLine();
-            Console.WriteLine("Invoking an indexed property (getting and setting.)");
-            Console.WriteLine("--------------------------------------------------");
-            // BindingFlags.GetProperty
-            // Get an indexed property value.
-            int index = 3;
-            result = t.InvokeMember("Item", BindingFlags.GetProperty, null, c, new object[] { index });
-            Console.WriteLine("Item[{0}] == {1}", index, result);
-            // BindingFlags.SetProperty
-            // Set an indexed property value.
-            index = 3;
-            t.InvokeMember("Item", BindingFlags.SetProperty, null, c, new object[] { index, "NewValue" });
-            result = t.InvokeMember("Item", BindingFlags.GetProperty, null, c, new object[] { index });
-            Console.WriteLine("Item[{0}] == {1}", index, result);
-
-            Console.WriteLine();
-            Console.WriteLine("Getting a field or property.");
-            Console.WriteLine("----------------------------");
-            // BindingFlags.GetField
-            // Get a field or property.
-            result = t.InvokeMember("Name", BindingFlags.GetField | BindingFlags.GetProperty, null, c,
-                new object[] { });
-            Console.WriteLine("Name == {0}", result);
-            // BindingFlags.GetProperty
-            result = t.InvokeMember("Value", BindingFlags.GetField | BindingFlags.GetProperty, null, c,
-                new object[] { });
-            Console.WriteLine("Value == {0}", result);
-
-            Console.WriteLine();
-            Console.WriteLine("Invoking a method with named parameters.");
-            Console.WriteLine("---------------------------------------");
-            // BindingFlags.InvokeMethod
-            // Call a method using named parameters.
-            object[] argValues = new object[] { 123.ToString(), "Micky" };
-            String[] argNames = new String[] { "lastName", "firstName" };
-            var method = t.GetMethod("PrintName", BindingFlags.Static | BindingFlags.Public);
-            method.Invoke(null, argValues);
-            t.InvokeMember("PrintName", BindingFlags.InvokeMethod, null, null, argValues, null, null,
-                argNames);
-
-            Console.WriteLine();
-            Console.WriteLine("Invoking a default member of a type.");
-            Console.WriteLine("------------------------------------");
-            // BindingFlags.Default
-            // Call the default member of a type.
-            Type t3 = typeof(TestClass2);
-            t3.InvokeMember("", BindingFlags.InvokeMethod | BindingFlags.Default, null, new TestClass2(),
-                new object[] { });
-
-            // BindingFlags.Static, NonPublic, and Public
-            // Invoking a member with ref parameters.
-            Console.WriteLine();
-            Console.WriteLine("Invoking a method with ref parameters.");
-            Console.WriteLine("--------------------------------------");
-            MethodInfo m = t.GetMethod("Swap");
-            args = new object[2];
-            args[0] = 1;
-            args[1] = 2;
-            m.Invoke(new TestClass(), args);
-            Console.WriteLine("{0}, {1}", args[0], args[1]);
-
-            // BindingFlags.CreateInstance
-            // Creating an instance with a parameterless constructor.
-            Console.WriteLine();
-            Console.WriteLine("Creating an instance with a parameterless constructor.");
-            Console.WriteLine("------------------------------------------------------");
-            object cobj = t.InvokeMember("TestClass", BindingFlags.Public |
-                BindingFlags.Instance | BindingFlags.CreateInstance,
-                null, null, new object[] { });
-            Console.WriteLine("Instance of {0} created.", cobj.GetType().Name);
-
-            // Creating an instance with a constructor that has parameters.
-            Console.WriteLine();
-            Console.WriteLine("Creating an instance with a constructor that has parameters.");
-            Console.WriteLine("------------------------------------------------------------");
-            cobj = t.InvokeMember("TestClass", BindingFlags.Public |
-                BindingFlags.Instance | BindingFlags.CreateInstance,
-                null, null, new object[] { "Hello, World!" });
-            Console.WriteLine("Instance of {0} created with initial value '{1}'.", cobj.GetType().Name,
-                cobj.GetType().InvokeMember("Name", BindingFlags.GetField, null, cobj, null));
-
-            // BindingFlags.DeclaredOnly
-            Console.WriteLine();
-            Console.WriteLine("DeclaredOnly instance members.");
-            Console.WriteLine("------------------------------");
-            System.Reflection.MemberInfo[] memInfo =
-                t.GetMembers(BindingFlags.DeclaredOnly | BindingFlags.Instance |
-                BindingFlags.Public);
-            for (int i = 0; i < memInfo.Length; i++)
-            {
-                Console.WriteLine(memInfo[i].Name);
-            }
-
-            // BindingFlags.IgnoreCase
-            Console.WriteLine();
-            Console.WriteLine("Using IgnoreCase and invoking the PrintName method.");
-            Console.WriteLine("---------------------------------------------------");
-            t.InvokeMember("printname", BindingFlags.IgnoreCase | BindingFlags.Static |
-                BindingFlags.Public | BindingFlags.InvokeMethod, null, null, new object[]
-                {"Brad","Smith"});
-
-            // BindingFlags.FlattenHierarchy
-            Console.WriteLine();
-            Console.WriteLine("Using FlattenHierarchy to get inherited static protected and public members.");
-            Console.WriteLine("----------------------------------------------------------------------------");
-            FieldInfo[] finfos = typeof(MostDerived).GetFields(BindingFlags.NonPublic | BindingFlags.Public |
-                  BindingFlags.Static | BindingFlags.FlattenHierarchy);
-            foreach (FieldInfo finfo in finfos)
-            {
-                Console.WriteLine("{0} defined in {1}.", finfo.Name, finfo.DeclaringType.Name);
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("Without FlattenHierarchy.");
-            Console.WriteLine("-------------------------");
-            finfos = typeof(MostDerived).GetFields(BindingFlags.NonPublic | BindingFlags.Public |
-                  BindingFlags.Static);
-            foreach (FieldInfo finfo in finfos)
-            {
-                Console.WriteLine("{0} defined in {1}.", finfo.Name, finfo.DeclaringType.Name);
-            }
+        private static void Printer_IncLeak(string incColor)
+        {
+            printer.Refill(incColor);
         }
 
         //MyArray<int> myArray = new MyArray<int>();
